0d6448ca396fda9f7394c53b2281bb2023ca5d50
Chinweike99 <innocentakwolu1@gmail.com>
2024-08-11 05:41:42 +0100

    updated

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -1,8 +1,19 @@
+import TickIcon from './TickIcon';
+import ProgressBar from './ProgressBar';
+
 function ListItem({task}) {
     return (
-      <div>
-        <p>{task.title} {task.progress} {task.date}</p>
-      </div>
+      <li className="list-item">
+        <div className='info-container'>
+          <TickIcon />
+          <p className="task-title">{task.title} {task.progress} {task.date}</p>
+          <ProgressBar />
+        </div>
+        <div className='button-container'>
+          <button className='edit'>EDIT</button>
+          <button className='deletes'>DELETE</button>
+        </div>
+      </li>
     );
   }
   
